66b3addb1b3b2246ada4432f2c29fdc2c19bdca4
Kelley Shackelford <kelleyalexis@yahoo.com>
2023-10-17 14:00:59 -0400

    modified DOM

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const mainElement = document.querySelector('main#main');
+
+if (mainElement) {
+    mainElement.parentNode.removeChild(mainElement);
+  }
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Kelley is the champion";
